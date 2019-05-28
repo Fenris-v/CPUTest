31c1cbb8c3e8819b72ec32caac05eb5bf4d6639a
Fenris-c <anatolijv236@gmail.com>
2019-05-28 13:56:30 +0300

    Add multiple threads

--- app/src/main/java/com/petersburg_studio/cputest/MainActivity.java
+++ app/src/main/java/com/petersburg_studio/cputest/MainActivity.java
@@ -1,29 +1,64 @@
 package com.petersburg_studio.cputest;
 
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean running = false;
     double count = 0.0;
+    int thread;
+    EditText editText;
+    Snackbar snackbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        runTest();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        editText = findViewById(R.id.number_of_threads);
+
     }
 
     public void onClickStart(View view) {
-        running = true;
+        runTest();
     }
 
-    public void onClickStop(View view) {
-        running = false;
+    public void onClickMultipleThread(View view) {
+        if (!editText.getText().toString().equals("")) {
+            thread = Integer.parseInt(editText.getText().toString());
+            if (thread > 0) {
+                runTestMultipleThread();
+            } else {
+                snackbar = Snackbar.make(view, "Number must be > 0", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Close", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                snackbar.dismiss();
+                            }
+                        });
+                snackbar.show();
+            }
+        } else {
+            snackbar = Snackbar.make(view, "Edit text don't must be empty", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Close", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            snackbar.dismiss();
+                        }
+                    });
+            snackbar.show();
+        }
     }
 
     private void runTest() {
@@ -31,12 +66,23 @@ public class MainActivity extends AppCompatActivity {
         handler.post(new Runnable() {
             @Override
             public void run() {
-                if (running) {
-                    while (count != 1.0) {
-                        count += 1.0 / 10;
-                        System.out.println("Value: " + count);
-                    }
-                } handler.postDelayed(this, 0);
+                while (count != 1.0) {
+                    count += 1.0 / 10;
+                }
+                handler.postDelayed(this, 0);
+            }
+        });
+    }
+
+    private void runTestMultipleThread() {
+        ExecutorService executorService = Executors.newFixedThreadPool(thread);
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                while (count != 1.0) {
+                    count += 1.0 / 10;
+                    System.out.println("Value: " + count);
+                }
             }
         });
     }
